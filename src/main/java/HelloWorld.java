7c63e57c99a0718a8d3287872b78895690bd563e
Benjamin Jerkel <jerkeben@b-tu.de>
2020-09-09 22:57:58 +0200

    Nachricht modifiziert und kommentiert

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -4,5 +4,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Guten Tag Benjamin");
+        System.out.println("Willkommen in der Veranstaltung \"Strukturierte Programmierung\""); // Alternative Schreibweise / Strukturierung ggnüber dem 1. Assignment
     }
 }
